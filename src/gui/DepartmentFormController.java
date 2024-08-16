3d42cf4a4307241ecfc2523ae093d42a540f2764
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-08-16 13:27:36 -0300

    Passando a entidade Department para o DepartmentForm view

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -8,9 +8,12 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import model.entities.Department;
 import util.Constraints;
 
 public class DepartmentFormController implements Initializable{
+	
+	private Department entity;
 	
 	@FXML
 	private TextField textId;
@@ -40,6 +43,18 @@ public class DepartmentFormController implements Initializable{
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		initializeNodes();
+	}
+	
+	public void setDepartment(Department entity) {
+		this.entity = entity;
+	}
+	
+	public void updateFromData() {
+		if(entity == null) {
+			throw new IllegalStateException("Entidade nula!");
+		}
+		textId.setText(String.valueOf(entity.getId()));
+		textName.setText(entity.getName());
 	}
 	
 	private void initializeNodes() {
--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -46,7 +46,8 @@ public class DepartmentListController implements Initializable{
 	@FXML
 	public void onButtonNewAction(ActionEvent event) {
 		Stage parentStage = Utils.currentStage(event);
-		createDialogForm("/gui/DepartmentForm.fxml", parentStage);
+		Department department = new Department();
+		createDialogForm(department, "/gui/DepartmentForm.fxml", parentStage);
 	}
 	
 	public void setDepartmentService(DepartmentService service) {
@@ -75,10 +76,14 @@ public class DepartmentListController implements Initializable{
 		tableViewDepartment.setItems(observableList);
 	}
 	
-	private void createDialogForm(String absoluteName, Stage parentStage) {
+	private void createDialogForm(Department department, String absoluteName, Stage parentStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 			Pane pane = loader.load();
+			
+			DepartmentFormController controller = loader.getController();
+			controller.setDepartment(department);
+			controller.updateFromData();
 			
 			Stage dialogStage = new Stage();
 			dialogStage.setTitle("Entre com os dados:");
